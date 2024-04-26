5b6cc3c93f7c560bff9960c52f4455acf8002e1b
Andre Pereira <andre.9494@gmail.com>
2024-04-26 15:09:34 -0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+export default function App() {
+  return (
+    // <View style={styles.container}>
+    <View style = {styles.container}>
+      <Text>Je suis tres content!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 50,
+    marginHorizontal: 10,
+  },
+  centeredContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
