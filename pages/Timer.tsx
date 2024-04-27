33c06a3ae540162410d9db47544f6109a4013a7e
Andre Pereira <andre.9494@gmail.com>
2024-04-27 13:30:42 +0100

    timer counting

--- pages/Timer.tsx
+++ pages/Timer.tsx
@@ -3,49 +3,61 @@ import Square from "../components/Square";
 import TextField from "../components/TextField";
 import styles from "../styles";
 import moment from "moment";
+import { useEffect, useState } from "react";
 
 const Timer = () => {
-  const currentDate = moment("2024-04-27");
+//   const currentDate = moment();
+  const currentDate = moment();
   const targetDate = moment("2025-05-31");
   const duration = moment.duration(targetDate.diff(currentDate));
-//   const date = moment(dateDiffMili);
-const years = duration.years();
-const months = duration.months();
-const days = duration.days();
-const hours = duration.hours();
-const minutes = duration.minutes();
-const seconds = duration.seconds();
-    return (
-    <View style={{...styles.pageContainer}}>
-      <TextField>Je suis tres content!!</TextField>
-      <TextField>{currentDate.format("yyyy/MM/DD-hh:mm:ss")}</TextField>
-      <TextField>{targetDate.format("yyyy/MM/DD-hh:mm:ss")}</TextField>
-      <TextField>{`years: ${years}, months: ${months}, days: ${days}`}</TextField>
-      <TextField>{`hours: ${hours}, minutes: ${minutes}, seconds: ${seconds}`}</TextField>
+  //   const years = duration.years();
+  //   const months = duration.months();
+  //   const days = duration.days();
+  //   const hours = duration.hours();
+  //   const minutes = duration.minutes();
+  //   const seconds = duration.seconds();
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    
+    setTimeout(()=>{setCount(count+1)}, 100);
+  }, [count]);
+  
+  return (
+	<View style={{ ...styles.pageContainer }}>
+	  <TextField>{`count: ${count}`}</TextField>
+	  <TextField>Je suis tres content!!</TextField>
+	  <TextField>{currentDate.format("yyyy/MM/DD-hh:mm:ss")}</TextField>
+	  <TextField>{targetDate.format("yyyy/MM/DD-hh:mm:ss")}</TextField>
+	  <TextField>{`years: ${duration.years()}, months: ${duration.months()}, days: ${duration.days()}`}</TextField>
+	  <TextField>{`hours: ${duration.hours()}, minutes: ${duration.minutes()}, seconds: ${duration.seconds()}`}</TextField>
 
-      <View style={{ ...styles.container, display: "flex", flexDirection: "row"}}>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-      </View>
-      <View style={{ ...styles.container, display: "flex", flexDirection: "row"}}>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-        <View style={styles.container}>
-          <Square>0</Square>
-        </View>
-      </View>
-    </View>
+	  <View
+		style={{ ...styles.container, display: "flex", flexDirection: "row" }}
+	  >
+		<View style={styles.container}>
+		  <Square>{duration.years()}</Square>
+		</View>
+		<View style={styles.container}>
+		  <Square>{duration.months()}</Square>
+		</View>
+		<View style={styles.container}>
+		  <Square>{duration.days()}</Square>
+		</View>
+	  </View>
+	  <View
+		style={{ ...styles.container, display: "flex", flexDirection: "row" }}
+	  >
+		<View style={styles.container}>
+		  <Square>{duration.hours()}</Square>
+		</View>
+		<View style={styles.container}>
+		  <Square>{duration.minutes()}</Square>
+		</View>
+		<View style={styles.container}>
+		  <Square>{duration.seconds()}</Square>
+		</View>
+	  </View>
+	</View>
   );
 };
 
